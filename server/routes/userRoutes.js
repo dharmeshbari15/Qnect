e67ec775f82654d80a5dce4b8fac36c3c42b594b
dharmeshbari15 <dharmeshbari15@gmail.com>
2025-07-12 16:02:06 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,41 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
-
 const {
-
   registerUser,
-
   loginUser,
-
   getUserProfile,
-
   updateUserProfile,
-
   getUsers
-
 } = require('../controllers/userController');
-
 const { protect } = require('../middleware/authMiddleware');
 
-
-
 // Public routes
-
 router.post('/register', registerUser);
-
 router.post('/login', loginUser);
-
 router.get('/', getUsers);
 
-
-
 // Protected routes
-
 router.get('/profile', protect, getUserProfile);
-
 router.put('/profile', protect, updateUserProfile);
-
-
 
 module.exports = router;
