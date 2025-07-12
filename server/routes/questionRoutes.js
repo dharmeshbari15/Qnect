2a1840b8106beb07a5cfc39c70c3849b155c94a7
dharmeshbari15 <dharmeshbari15@gmail.com>
2025-07-12 16:03:41 +0530

    Update questionRoutes.js

--- server/routes/questionRoutes.js
+++ server/routes/questionRoutes.js
@@ -1,49 +1,25 @@
 const express = require('express');
-
 const router = express.Router();
-
 const {
-
   getQuestions,
-
   getQuestion,
-
   createQuestion,
-
   updateQuestion,
-
   deleteQuestion,
-
   voteQuestion,
-
   getPopularTags
-
 } = require('../controllers/questionController');
-
 const { protect } = require('../middleware/authMiddleware');
 
-
-
 // Public routes
-
 router.get('/', getQuestions);
-
 router.get('/tags', getPopularTags);
-
 router.get('/:id', getQuestion);
 
-
-
 // Protected routes
-
 router.post('/', protect, createQuestion);
-
 router.put('/:id', protect, updateQuestion);
-
 router.delete('/:id', protect, deleteQuestion);
-
 router.put('/:id/vote', protect, voteQuestion);
-
-
 
 module.exports = router;
